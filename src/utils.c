7b9fc9876183befefe0bf2543669f6fe16ae5f2c
renan823 <renan.trofino@gmail.com>
2025-06-08 09:28:52 -0300

    testes

--- src/utils.c
+++ src/utils.c
@@ -7,37 +7,44 @@
 // Função para ler e processar campos numéricos inteiros
 int LerCampoInteiro(void) {
     char* campo = LerString();
-		int valor = -1; // Valor NULO
-		// Preencher inteiro 
-    if (strcmp(campo, "NULO") != 0) valor = atoi(campo);
+	int valor = -1; // Valor NULO
+
+	// Preencher inteiro 
+    if (strcmp(campo, "NULO") != 0) {
+		valor = atoi(campo);
+	}
 
     free(campo);
-		campo = NULL;
+	campo = NULL;
 
-		return valor;
+	return valor;
 }
 
 // Função para ler e processar campos numéricos float
 float LerCampoFloat(void) {
     char* campo = LerString();
-		float valor = -1; // Valor NULO
-		// Preencher float 
-    if (strcmp(campo, "NULO") != 0) valor = strtof(campo, NULL);
+	float valor = -1; // Valor NULO
+
+	// Preencher float 
+    if (strcmp(campo, "NULO") != 0) {
+		valor = strtof(campo, NULL);
+	}
 
     free(campo);
-		campo = NULL;
+	campo = NULL;
 
-		return valor;
+	return valor;
 }
 
 // Função para ler e processar campos de string com aspas
 char *LerCampoStringComAspas(void) {
-		char* campo = LerStringComAspas();
-		if (strcmp(campo, "") == 0) {
-			free(campo);
-			campo = NULL;
-		}
-		return campo;
+	char* campo = LerStringComAspas();
+	if (strcmp(campo, "") == 0) {
+		free(campo);
+		campo = NULL;
+	}
+	
+	return campo;
 }
 
 // Função lê uma string sem desperdício de memória
